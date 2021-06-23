aacb34162f644dbbf08f6112ccb37bbe2e677450
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-06-23 21:31:23 +0200

    fix: button hover , active background-color

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -152,7 +152,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52583" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64309" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
